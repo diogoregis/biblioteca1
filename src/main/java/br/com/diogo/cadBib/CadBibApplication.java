35f6ed46c7675442d59d82abcc6f92729a5ee2ac
Diogo Regis <diogo.regis.silva@gmail.com>
2024-06-23 15:30:43 -0300

    iniciando projeto

---
+++ src/main/java/br/com/diogo/cadBib/CadBibApplication.java
@@ -0,0 +1,13 @@
+package br.com.diogo.cadBib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadBibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadBibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/diogo/cadBib/CadBibApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diogo.cadBib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadBibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
