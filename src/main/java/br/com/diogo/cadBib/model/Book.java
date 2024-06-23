649319302a0eb036cb406871779e684e58ce3cea
Diogo Regis <diogo.regis.silva@gmail.com>
2024-06-23 15:36:39 -0300

    criando e configurando class Book, pacote model

---
+++ src/main/java/br/com/diogo/cadBib/model/Book.java
@@ -0,0 +1,22 @@
+package br.com.diogo.cadBib.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private String isbn;
+    private int year;
+
+}
